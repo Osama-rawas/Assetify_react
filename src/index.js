2c91b19b82224e854c649850e5364701d454db3c
unknown <mylearncourses70@gmail.com>
2024-06-22 00:45:36 +0300

    deply

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/Assetify">
+  <BrowserRouter basename="/">
     <React.StrictMode>
       <Router>
         <App />
